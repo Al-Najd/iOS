667f05b0dc3dfe7d078785de7315d0150bd1885c
Ahmed Ramy <Dev.AhmedRamy@gmail.com>
2021-10-13 10:12:09 +0200

    Initial Commit

---
+++ Shared/The_OneApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_OneApp.swift
+//  Shared
+//
+//  Created by Ahmed Ramy on 13/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct The_OneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
