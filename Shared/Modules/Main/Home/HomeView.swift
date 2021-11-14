c2d75d037111601ee61ca2fe001b856456caae61
Ahmed Ramy <dev.ahmedramy@gmail.com>
2021-11-14 04:50:51 +0200

    Remove Dead Code

--- Shared/Modules/Main/Home/HomeView.swift
+++ Shared/Modules/Main/Home/HomeView.swift
@@ -135,63 +135,6 @@ struct BuffCardView: View {
   }
 }
 
-struct DeedRowView: View {
-  var deed: Deed
-  @State var progress: CGFloat = 0
-  @State var isPressing: Bool = false
-  @State var duration: CGFloat = 2
-  
-  var body: some View {
-    GeometryReader { geo in
-      ZStack(alignment: .leading) {
-        HStack(alignment: .center) {
-          if deed.isDone {
-            Image(systemName: "checkmark.seal.fill")
-              .foregroundColor(.success.default)
-          }
-          Text(deed.title)
-            .font(.pBody)
-            .foregroundColor(deed.isDone ? .mono.offwhite : .mono.offblack)
-        }
-        .padding(.horizontal, .p32)
-        .frame(maxWidth: .infinity, alignment: .center)
-        .padding(.vertical, .p16)
-        .background(
-          RoundedRectangle(cornerRadius: .r16)
-            .fill(Color.primary1.default.opacity(deed.isDone ? 0.3 : 1))
-            .frame(
-              width: (geo.size.width * progress).clamped(to: 0...geo.size.width),
-              alignment: .leading
-            )
-        )
-        .background(deed.isDone ? Color.primary1.default.opacity(0.75) : Color.primary1.background)
-        .cornerRadius(.r16)
-        .onTapGesture { } // To avoid conflicts with Drag Gesture of the list
-        .onLongPressGesture(minimumDuration: duration) { isPressing in
-          if isPressing {
-            withAnimation(.linear(duration: duration)) {
-              progress = deed.isDone ? 0 : 1
-            }
-            HapticService.main.generateHoldFeedback(for: duration)
-          } else {
-            withAnimation(.linear(duration: duration/2)) {
-              progress = deed.isDone ? 1 : 0
-            }
-            HapticService.main.generateHoldFeedback(for: duration/2)
-          }
-        } perform: {
-          progress = deed.isDone ? 0 : 1
-          HapticService.main.generate(feedback: .success)
-          withAnimation(.easeInOut) {
-            app.handle(deed: deed)
-            MusicService.main.start(effect: .splashEnd)
-          }
-        }
-      }
-    }
-  }
-}
-
 extension Comparable {
     func clamped(to limits: ClosedRange<Self>) -> Self {
         return min(max(self, limits.lowerBound), limits.upperBound)
