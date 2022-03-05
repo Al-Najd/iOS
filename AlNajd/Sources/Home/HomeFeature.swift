e4460b80e3634b5a58b43cbe9b0b1773866dd1f4
Ahmed Ramy <dev.ahmedramy@gmail.com>
2022-03-05 13:11:03 +0200

    Implement UI of Home

--- AlNajd/Sources/Home/HomeFeature.swift
+++ AlNajd/Sources/Home/HomeFeature.swift
@@ -6,20 +6,20 @@
 //
 
 import ComposableArchitecture
-import Dashboard
-import Prayers
-import Azkar
-import Rewards
-import Date
-import Settings
-import Location
+//import Dashboard
+//import Prayers
+//import Azkar
+//import Rewards
+//import Date
+//import Settings
+//import Location
 
 struct HomeFeature: Equatable {
-  var locationState: LocationState
-  var dashboardState: DashboardState
-  var prayerState: PrayerState
-  var azkarState: AzkarState
-  var rewardState: RewardsState
-  var dateState: DateState
-  var settingsState: SettingsState
+//  var locationState: LocationState
+//  var dashboardState: DashboardState
+//  var prayerState: PrayerState
+//  var azkarState: AzkarState
+//  var rewardState: RewardsState
+//  var dateState: DateState
+//  var settingsState: SettingsState
 }
--- AlNajd/Sources/Home/HomeView.swift
+++ AlNajd/Sources/Home/HomeView.swift
@@ -7,11 +7,12 @@
 
 import SwiftUI
 import Common
+import DesignSystem
 
 struct HomeView: View {
     var body: some View {
       ScrollView(.vertical, showsIndicators: false) {
-        Group {
+        VStack(spacing: .p16) {
         HStack {
           VStack(alignment: .leading) {
             Text("28 Rajab, 1443 AH")
@@ -28,8 +29,48 @@ struct HomeView: View {
             details: "Don't get distracted by Dunia, it's temporarily, and never forget why you're here, you're here to build yourself a place in Jannah"
           )
         )
+          Text("Prayer")
+            .scaledFont(.pTitle1, .bold)
           
+          VStack {
+            Text("Duhr with be out in 30 Minutes")
+              .scaledFont(.pHeadline, .bold)
+            
+            InsightCardView(
+              insight: .init(indicator: .encourage, details: "If you prepare now, you will be able to pray in first line and pray Sunnah and Tahyt Al Masjd")
+            )
+          }.fillOnLeading()
           
+          VStack {
+            Text("Azkar")
+              .scaledFont(.pTitle2, .bold)
+            Text("We have around 20 zekr, how about you do five of them now?")
+              .scaledFont(.pHeadline, .bold)
+              .multilineTextAlignment(.center)
+            
+            Button {
+              // Route to Azkar's Page
+            } label: {
+              Text("Let's do them!")
+                .scaledFont(.pBody, .bold)
+                .foregroundColor(.mono.offwhite)
+                .padding()
+                .background(RoundedRectangle(cornerRadius: .r12))
+            }.fillAndCenter()
+          }.fillOnLeading()
+          
+          Text("Latest Reward")
+            .scaledFont(.pTitle1, .bold)
+            .fillOnLeading()
+          VStack {
+            Text("Reward Name")
+            Text("Reward Description")
+          }.fillAndCenter()
+            .padding()
+            .background(
+              RoundedRectangle(cornerRadius: .r12)
+                .foregroundColor(.primary.default)
+            )
         }.padding(.horizontal)
       }
     }
