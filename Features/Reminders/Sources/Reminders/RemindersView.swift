2ff8d942148b602163bde042795be2bf79f041f0
Ahmed Ramy <Dev.AhmedRamy@gmail.com>
2023-01-23 17:26:11 +0200

    feat: Add Design System 🎨 (#57)

---
+++ Features/Reminders/Sources/Reminders/RemindersView.swift
@@ -0,0 +1,21 @@
+//
+//  RemindersView.swift
+//  Reminders
+//
+//  Created by Ahmed Ramy on 23/01/2023.
+//  Copyright © 2023 Al Najd. All rights reserved.
+//
+
+import SwiftUI
+import uDesignSystem
+
+public struct RemindersView: View {
+    public init() { }
+
+    public var body: some View {
+        VStack {
+            Text("Hello, Reminders!")
+            Spacer()
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
---
+++ Features/uDesignSystem/Sources/Font+extension.swift
@@ -0,0 +1,133 @@
+//  swiftlint:disable all
+//
+//  The code generated using FigmaExport — Command line utility to export
+//  colors, typography, icons and images from Figma to Xcode project.
+//
+//  https://github.com/RedMadRobot/figma-export
+//
+//  Don’t edit this code manually to avoid runtime crashes
+//
+
+import SwiftUI
+
+public extension Font {
+    /// Weight: **Regular**, Size: **32**
+    static func mobileDisplayHuge() -> Font {
+        Font.custom("Poppins-Regular", size: 32.0)
+    }
+
+    /// Weight: **Bold**, Size: **32**
+    static func mobileDisplayHugeBold() -> Font {
+        Font.custom("Poppins-Bold", size: 32.0)
+    }
+
+    /// Weight: **Regular**, Size: **28**
+    static func mobileDisplayLarge() -> Font {
+        Font.custom("Poppins-Regular", size: 28.0)
+    }
+
+    /// Weight: **Bold**, Size: **28**
+    static func mobileDisplayLargeBold() -> Font {
+        Font.custom("Poppins-Bold", size: 28.0)
+    }
+
+    /// Weight: **Regular**, Size: **24.0**
+    static func mobileDisplayMedium() -> Font {
+        Font.custom("Poppins-Regular", size: 24.0)
+    }
+
+    /// Weight: **Bold**, Size: **24.0**
+    static func mobileDisplayMediumBold() -> Font {
+        Font.custom("Poppins-Bold", size: 24.0)
+    }
+
+    /// Weight: **Regular**, Size: **20.0**
+    static func mobileDisplaySmall() -> Font {
+        Font.custom("Poppins-Regular", size: 20.0)
+    }
+
+    /// Weight: **Bold**, Size: **20.0**
+    static func mobileDisplaySmallBold() -> Font {
+        Font.custom("Poppins-Bold", size: 20.0)
+    }
+
+    /// Weight: **Bold**, Size: **20.0**
+    static func mobileLinkLarge() -> Font {
+        Font.custom("Poppins-Bold", size: 20.0)
+    }
+
+    /// Weight: **Bold**, Size: **20.0**
+    static func mobileLinkLargeTight() -> Font {
+        Font.custom("Poppins-Bold", size: 20.0)
+    }
+
+    /// Weight: **Bold**, Size: **17.0**
+    static func mobileLinkMedium() -> Font {
+        Font.custom("Poppins-Bold", size: 17.0)
+    }
+
+    /// Weight: **Bold**, Size: **17.0**
+    static func mobileLinkMediumTight() -> Font {
+        Font.custom("Poppins-Bold", size: 17.0)
+    }
+
+    /// Weight: **Bold**, Size: **15.0**
+    static func mobileLinkSmall() -> Font {
+        Font.custom("Poppins-Bold", size: 15.0)
+    }
+
+    /// Weight: **Bold**, Size: **15.0**
+    static func mobileLinkSmallTight() -> Font {
+        Font.custom("Poppins-Bold", size: 15.0)
+    }
+
+    /// Weight: **Bold**, Size: **13.0**
+    static func mobileLinkXSmall() -> Font {
+        Font.custom("Poppins-Bold", size: 13.0)
+    }
+
+    /// Weight: **Bold**, Size: **13.0**
+    static func mobileLinkXSmallTight() -> Font {
+        Font.custom("Poppins-Bold", size: 13.0)
+    }
+
+    /// Weight: **Regular**, Size: **20.0**
+    static func mobileTextLarge() -> Font {
+        Font.custom("Poppins-Regular", size: 20.0)
+    }
+
+    /// Weight: **Regular**, Size: **20.0**
+    static func mobileTextLargeTight() -> Font {
+        Font.custom("Poppins-Regular", size: 20.0)
+    }
+
+    /// Weight: **Regular**, Size: **17.0**
+    static func mobileTextMedium() -> Font {
+        Font.custom("Poppins-Regular", size: 17.0)
+    }
+
+    /// Weight: **Regular**, Size: **17.0**
+    static func mobileTextMediumTight() -> Font {
+        Font.custom("Poppins-Regular", size: 17.0)
+    }
+
+    /// Weight: **Regular**, Size: **15.0**
+    static func mobileTextSmall() -> Font {
+        Font.custom("Poppins-Regular", size: 15.0)
+    }
+
+    /// Weight: **Regular**, Size: **15.0**
+    static func mobileTextSmallTight() -> Font {
+        Font.custom("Poppins-Regular", size: 15.0)
+    }
+
+    /// Weight: **Medium**, Size: **13.0**
+    static func mobileTextXSmall() -> Font {
+        Font.custom("Poppins-Medium", size: 13.0)
+    }
+
+    /// Weight: **Medium**, Size: **13.0**
+    static func mobileTextXSmallTight() -> Font {
+        Font.custom("Poppins-Medium", size: 13.0)
+    }
+}
