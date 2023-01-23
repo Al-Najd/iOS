134fafa9a33781d9d3feb71631754c32c2726971
Ahmed Ramy <dev.ahmedramy@gmail.com>
2023-01-24 00:53:34 +0200

    Add UI

--- Features/Reminders/Sources/Reminders/RemindersView.swift
+++ Features/Reminders/Sources/Reminders/RemindersView.swift
@@ -12,27 +12,67 @@ import uDesignSystem
 // MARK: - RemindersView
 
 public struct RemindersView: View {
+    @State var progress = 1.0
+    @State var text = "00:00"
+
     public init() { }
 
     public var body: some View {
         VStack {
             Text("5 Mins Of Zekr")
                 .font(.mobileDisplayLarge())
+                .bold()
                 .foregroundColor(.grayscaleBackground)
 
             GeometryReader { proxy in
                 VStack(spacing: .s16) {
                     ZStack {
                         Circle()
+                            .fill(Color.grayscaleBackground.opacity(0.03))
+                            .padding(-.s40)
+
+                        Circle()
+                            .trim(from: 0, to: progress)
+                            .stroke(Color.grayscaleBackground.opacity(0.03), lineWidth: .s40 + .s40)
+                            .blur(radius: .r16)
+                            .padding(-.s4)
+
+                        // MARK: - Shadow
+                        Circle()
+                            .stroke(
+                                Color.primaryDefaultWeak,
+                                lineWidth: 5)
+                            .blur(radius: .r16)
+                            .padding(-.s4)
+
+                        Circle()
+                            .fill(Color.grayscaleHeaderWeak)
+                            .blur(radius: .r16)
+                            .padding(-.s4)
+
+                        Circle()
+                            .trim(from: 0, to: progress)
+                            .stroke(
+                                Color.primaryDefault.opacity(0.7),
+                                lineWidth: 10)
+
+                        Text(text)
+                            .font(.mobileDisplayHuge())
+                            .bold()
+                            .rotationEffect(.degrees(90))
                     }
                     .padding(.s8 * 8)
                     .frame(width: proxy.size.width)
+                    .rotationEffect(.degrees(-90))
+                    .animation(.easeInOut, value: progress)
                 }
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
             }
         }
         .padding()
-        .background(Color.grayscaleHeader)
+        .background {
+            Color.grayscaleHeaderWeak.ignoresSafeArea()
+        }
         .preferredColorScheme(.dark)
     }
 }
@@ -42,5 +82,14 @@ public struct RemindersView: View {
 struct RemindersView_Previews: PreviewProvider {
     static var previews: some View {
         RemindersView()
+    }
+}
+
+// MARK: - RemindersView.UIModel
+
+extension RemindersView {
+    struct UIModel {
+        @State var progress: CGFloat = 1
+        @State var timeLeft = "00:00"
     }
 }
