8230772338b5262bfc2befcfdebf509b49e07506
Ahmed Ramy <dev.ahmedramy@gmail.com>
2023-03-28 06:40:24 +0200

    Reset Root View

--- Shared/Modules/Root/RootView.swift
+++ Shared/Modules/Root/RootView.swift
@@ -13,15 +13,42 @@ import Home
 import Localization
 import PrayerDetails
 import SwiftUI
-import Azkar
-import Inject
 
 // MARK: - RootView
 
 public struct RootView: View {
-  public let store: StoreOf<Root>
+    public let store: StoreOf<Root>
 
-  public var body: some View {
-    AzkarView()
-  }
+    public var body: some View {
+        WithViewStore(store) { viewStore in
+            TabView {
+              HomeView(store: store.scope(state: \.home, action: Root.Action.home))
+                    .tabItem {
+                        VStack {
+                            Image(systemName: "sun.dust.circle.fill")
+                            Text(L10n.today)
+                        }
+                        .foregroundColor(Asset.Colors.Primary.blackberry.swiftUIColor)
+                    }
+
+              DashboardView(store: store.scope(state: \.dashboard, action: Root.Action.dashboard))
+                    .tabItem {
+                        VStack {
+                            Image(systemName: "chart.bar.xaxis")
+                            Text(L10n.dashboard)
+                        }
+                        .foregroundColor(Asset.Colors.Primary.blackberry.swiftUIColor)
+                    }
+            }.onAppear {
+                // correct the transparency bug for Tab bars
+                let tabBarAppearance = UITabBarAppearance()
+                tabBarAppearance.configureWithOpaqueBackground()
+                UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+                // correct the transparency bug for Navigation bars
+                let navigationBarAppearance = UINavigationBarAppearance()
+                navigationBarAppearance.configureWithOpaqueBackground()
+                UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
+            }
+        }
+    }
 }
